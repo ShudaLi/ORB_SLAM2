a2564dd413fcceb60f239a80f8722d639afcc791
Shuda Li <lishuda1980@gmail.com>
2017-10-17 22:40:13 +0100

    minor changes

--- src/ORBmatcher.cc
+++ src/ORBmatcher.cc
@@ -1652,7 +1652,7 @@ int ORBmatcher::DescriptorDistance(const cv::Mat &a, const cv::Mat &b)
 
     int dist=0;
 
-    for(int i=0; i<8; i++, pa++, pb++)
+    for(int i=0; i<a.cols/sizeof(int32_t); i++, pa++, pb++)
     {
         unsigned  int v = *pa ^ *pb;
         v = v - ((v >> 1) & 0x55555555);
